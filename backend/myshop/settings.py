943aa48cbc3e6f66b01abfa14eb61fbb8c1f7342
FrancelLeBoss <cabrelprowo@gmail.com>
2026-07-20 00:25:10 -0400

    config mail avec reseend

--- backend/myshop/settings.py
+++ backend/myshop/settings.py
@@ -83,6 +83,7 @@ INSTALLED_APPS = [
     "api.apps.ApiConfig",
     "rest_framework.authtoken",
     "rest_framework_simplejwt",
+    "anymail",
 ]
 
 MIDDLEWARE = [
@@ -262,13 +263,19 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 if DEBUG:
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 else:
-    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-    EMAIL_HOST = "smtp.gmail.com"
-    EMAIL_PORT = 587
-    EMAIL_USE_TLS = True
-    EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-    EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-    EMAIL_TIMEOUT = int(os.getenv("EMAIL_TIMEOUT", "10"))
+    EMAIL_BACKEND = "anymail.backends.resend.EmailBackend"
+    ANYMAIL = {
+        "RESEND_API_KEY": os.getenv("RESEND_API_KEY"),
+    }
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL") or "noreply@francel-prowo.com"
+# else:
+#     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+#     EMAIL_HOST = "smtp.gmail.com"
+#     EMAIL_PORT = 587
+#     EMAIL_USE_TLS = True
+#     EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+#     EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+#     EMAIL_TIMEOUT = int(os.getenv("EMAIL_TIMEOUT", "10"))
 
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL") or os.getenv("EMAIL_HOST_USER") or "noreply@shopsy.com"
 
